cbd1384941722458df428ddab7fa31876d574c9c
mbekioui <mbekioui@excilys.com>
2013-06-04 14:45:32 +0200

    CompanyDao now uses JdbcTemplate

--- src/main/java/com/excilys/cdb/dao/CompanyDaoImpl.java
+++ src/main/java/com/excilys/cdb/dao/CompanyDaoImpl.java
@@ -1,13 +1,11 @@
 package com.excilys.cdb.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.excilys.cdb.pojo.Company;
@@ -18,62 +16,19 @@ public class CompanyDaoImpl implements ICompanyDao {
 	private static final String SQL_FIND = "SELECT id,name FROM company WHERE id = ?";
 	private static final String SQL_LIST = "SELECT id,name FROM company ORDER BY name";
 
+	private JdbcTemplate jdbcTemplate;
+
 	@Autowired
-	private DaoFactory daoFactory;
+	public void setJdbcTemplate(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
 
 	public Company find(int id) throws DaoException {
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		ResultSet resultSet = null;
-		Company company = null;
-
-		connection = daoFactory.getConnection();
-		try {
-			preparedStatement = DaoUtils.getPreparedStatement(connection,
-					SQL_FIND, false, id);
-			resultSet = preparedStatement.executeQuery();
-			if (resultSet.next()) {
-				company = map(resultSet);
-			}
-		} catch (SQLException e) {
-			throw new DaoException("CompanyDao@find() failed !", e);
-		} finally {
-			DaoUtils.silentClosing(preparedStatement, resultSet);
-		}
-
-		return company;
+		return jdbcTemplate.queryForObject(SQL_FIND, new Object[] { id },
+				new CompanyRowMapper());
 	}
 
 	public List<Company> list() throws DaoException {
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		ResultSet resultSet = null;
-		List<Company> companies = new ArrayList<Company>();
-
-		connection = daoFactory.getConnection();
-		try {
-			preparedStatement = DaoUtils.getPreparedStatement(connection,
-					SQL_LIST, false);
-			resultSet = preparedStatement.executeQuery();
-			while (resultSet.next()) {
-				companies.add(map(resultSet));
-			}
-		} catch (SQLException e) {
-			throw new DaoException("CompanyDao@list() failed !", e);
-		} finally {
-			DaoUtils.silentClosing(preparedStatement, resultSet);
-		}
-
-		return companies;
+		return jdbcTemplate.query(SQL_LIST, new CompanyRowMapper());
 	}
-
-	private Company map(ResultSet resultSet) throws SQLException {
-		Company company = new Company();
-
-		company.setId(resultSet.getInt(ID_FIELD));
-		company.setName(resultSet.getString(NAME_FIELD));
-
-		return company;
-	}
-
 }
